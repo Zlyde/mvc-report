ecdf77126014f7b1d250f35a24d6b3cee52512c4
Anton Lindahl <lindahl.anton@outlook.com>
2024-04-10 15:04:17 +0200

    first page

---
+++ app/src/Controller/MeControllerTwig.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MeControllerTwig extends AbstractController
+{
+    #[Route("/", name: "me")]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        $data = [
+            'number' => $number
+        ];
+
+        return $this->render('me.html.twig', $data);
+    }
+
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+
+    #[Route("/report", name: "report")]
+    public function home(): Response
+    {
+        return $this->render('report.html.twig');
+    }
+}
